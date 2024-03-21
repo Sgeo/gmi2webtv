ee45602aca5c4885a762e73245e383084f0b2b28
Sgeo <sgeoster@gmail.com>
2024-03-21 18:35:22 -0400

    Use character references instead of Windows-1252 (compatible with both WebTV and modern browsers), use RGI_Emoji regex to find all emoji (should catch multi-codepoint sequences), always strip out emoji variant selectors.

--- gmi2webtv.js
+++ gmi2webtv.js
@@ -2,42 +2,72 @@ function escapeHtml(unsafe) {
     return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
 }
 
-const WINDOWS_1252_BYTES = new Uint8Array(256);
-for(let i = 0; i < 256; i++) {
-    WINDOWS_1252_BYTES[i] = i;
-}
-let Windows1252Decoder = new TextDecoder("windows-1252");
-const WINDOWS_1252_CHARS = Windows1252Decoder.decode(WINDOWS_1252_BYTES);
-const STR_TO_WINDOWS_1252 = {};
-for(let i = 0; i < 256; i++) {
-    STR_TO_WINDOWS_1252[WINDOWS_1252_CHARS[i]] = i;
+const CHARCODES = {};
+// From https://html.spec.whatwg.org/multipage/parsing.html#numeric-character-reference-end-state
+CHARCODES[`€`] = 0x80; // Wrongly shows up in WebTV as right arrow
+CHARCODES[`‚`] = 0x82;
+CHARCODES[`ƒ`] = 0x83;
+CHARCODES[`„`] = 0x84;
+CHARCODES[`…`] = 0x85;
+CHARCODES[`†`] = 0x86;
+CHARCODES[`‡`] = 0x87;
+CHARCODES[`ˆ`] = 0x88;
+CHARCODES[`‰`] = 0x89;
+CHARCODES[`Š`] = 0x8A;
+CHARCODES[`‹`] = 0x8B;
+CHARCODES[`Œ`] = 0x8C;
+CHARCODES[`Ž`] = 0x8E; // Missing from WebTV
+CHARCODES[`‘`] = 0x91;
+CHARCODES[`’`] = 0x92;
+CHARCODES[`“`] = 0x93;
+CHARCODES[`”`] = 0x94;
+CHARCODES[`•`] = 0x95;
+CHARCODES[`–`] = 0x96;
+CHARCODES[`—`] = 0x97;
+CHARCODES[`˜`] = 0x98;
+CHARCODES[`™`] = 0x99;
+CHARCODES[`š`] = 0x9A;
+CHARCODES[`›`] = 0x9B;
+CHARCODES[`œ`] = 0x9C;
+CHARCODES[`ž`] = 0x9E; // Missing from WebTV
+CHARCODES[`Ÿ`] = 0x9F;
+
+
+function text2charrefs(text) {
+    let result = "";
+    for(let char of text) {
+        let codepoint = char.codePointAt(0);
+        if(codepoint >= 0x80) {
+            let charCode = CHARCODES[char];
+            if(!charCode) {
+                charCode = codepoint;
+            }
+            result += `&#${charCode};`;
+        } else {
+            result += char;
+        }
+    }
+    return result;
 }
 
-function get_emoji(emoji) {
+function getEmoji(emoji) {
     // Based on https://openmoji.org/faq/
     let emoji_code = [...emoji].map(e => e.codePointAt(0).toString(16).padStart(4, '0')).join(`-`).toUpperCase()
-    if (emoji_code.length === 10) emoji_code = emoji_code.replace("-FE0F", "");
+    emoji_code = emoji_code.replace("-FE0F", "");
     let new_url = `openmoji/${emoji_code}.gif`
     return `<img src=${new_url} height=16>`;
 }
 
-function text2windows1252(text) {
-    let resultBytes = [];
-    for(let codepoint of text) {
-        let winbyte = STR_TO_WINDOWS_1252[codepoint];
-        if(winbyte) {
-            resultBytes.push(winbyte);
-        } else if(!winbyte && !codepoint) {
-            resultBytes.push(0x00);
-        } else if(/\p{Emoji}/u.test(codepoint)) {
-            // This won't work well for multi-codepoint emojis. What happens to selectors? They get turned into dead images!
-            let emoji_img = get_emoji(codepoint);
-            resultBytes = resultBytes.concat([...emoji_img].map(c => c.codePointAt(0)));
-        } else {
-            resultBytes.push(0x00);
-        }
-    }
-    return new Uint8Array(resultBytes);
+function replaceEmoji(text) {
+    return text.replaceAll(/\p{RGI_Emoji}/gv, getEmoji);
+}
+
+function processText(text) {
+    // For text in main body
+    let escaped = escapeHtml(text);
+    let emojified = replaceEmoji(escaped); // TODO: Insert emoji processing here
+    let encoded = text2charrefs(emojified);
+    return encoded;
 }
 
 function gmi2webtv(gemtext) {
@@ -57,23 +87,23 @@ function gmi2webtv(gemtext) {
             preformatted = true;
             resultHtml += "<pre>\n";
         } else if(line.startsWith("### ")) {
-            resultHtml += `<h3>${escapeHtml(line)}</h3>\n`;
+            resultHtml += `<h3>${processText(line)}</h3>\n`;
         } else if(line.startsWith("## ")) {
-            resultHtml += `<h2>${escapeHtml(line)}</h2>\n`;
+            resultHtml += `<h2>${processText(line)}</h2>\n`;
         } else if(line.startsWith("# ")) {
-            resultHtml += `<h1>${escapeHtml(line)}</h1>\n`;
+            resultHtml += `<h1>${processText(line)}</h1>\n`;
         } else if(line.startsWith("=>")) {
             let [full, url, desc] = LINK_REGEX.exec(line);
             if(desc) {
-                resultHtml += `<p><a href="${escapeHtml(url)}">${escapeHtml(desc)}</a></p>\n`;
+                resultHtml += `<p><a href="${escapeHtml(url)}">${processText(desc)}</a></p>\n`;
             } else {
-                resultHtml += `<p><a href="${escapeHtml(url)}">${escapeHtml(url)}</a></p>\n`;
+                resultHtml += `<p><a href="${escapeHtml(url)}">${processText(url)}</a></p>\n`;
             }
         } else {
-            resultHtml += `<p>${escapeHtml(line)}<\p>\n`
+            resultHtml += `<p>${processText(line)}<\p>\n`
         }
     }
-    return text2windows1252(resultHtml);
+    return resultHtml;
 }
 
 export { gmi2webtv };
